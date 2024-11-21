bd0c77244114e9de475fad232d040b68c0eb9b4d
3garfield5 <matveykakozyrev@gmail.com>
2024-11-21 12:37:00 +0300

    add few tasks in task5

--- tasks/task5.java
+++ tasks/task5.java
@@ -1,6 +1,8 @@
 package tasks;
 
-import java.util.HashMap;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class task5 {
     public static void main(String[] args) {
@@ -23,6 +25,11 @@ public class task5 {
         System.out.println(digitsCount(12345));
         System.out.println(digitsCount(1289396387328L));
 
+        System.out.println("--Задача 4--");
+        System.out.println(totalPoints(new String[] {"cat", "create", "sat"}, "caster"));
+        System.out.println(totalPoints(new String[] {"trance", "recant"}, "recant"));
+        System.out.println(totalPoints(new String[] {"dote", "dotes", "toes", "set", "dot", "dots", "sted"}, "tossed"));
+
         System.out.println("--Задача 5--");
         System.out.println(longestRun(new int[] {1, 2, 3, 10, 11, 15}));
         System.out.println(longestRun(new int[] {5, 4, 2, 1}));
@@ -43,6 +50,23 @@ public class task5 {
         System.out.println(canMove("ладья", "A8", "H8"));
         System.out.println(canMove("слон", "A7", "G1"));
         System.out.println(canMove("ферзь", "C4", "D6"));
+
+        System.out.println("--Задача 8--");
+        System.out.println(maxPossible(9328, 456));
+        System.out.println(maxPossible(523, 76));
+        System.out.println(maxPossible(9132, 5564));
+        System.out.println(maxPossible(8732, 91255));
+
+        System.out.println("--Задача 9--");
+        System.out.println(timeDifference("Los Angeles", "April 1, 2011 23:23", "Canberra"));
+        System.out.println(timeDifference("London", "July 31, 1983 23:01", "Rome"));
+        System.out.println(timeDifference("New York", "December 31, 1970 13:40", "Beijing"));
+
+        System.out.println("--Задача 10--");
+        System.out.println(isNew(3));
+        System.out.println(isNew(30));
+        System.out.println(isNew(321));
+        System.out.println(isNew(123));
 
     }
 
@@ -148,13 +172,136 @@ public class task5 {
             return (dx <= 1 && dy <= 1);
 
         } else {
-            throw new IllegalArgumentException("Unknown chess piece: " + name);
+            throw new IllegalArgumentException("Неизвестная фигура: " + name);
         }
 
     }
 
-//    public static String maxPossible(int first, int second) {
-//        String firstStr = Integer.toString(first);
-//        return firstStr;
-//    }
+    public static int maxPossible(int first, int second) {
+        char[] firstChar = String.valueOf(first).toCharArray();
+
+        List<Integer> secondList = new ArrayList<>();
+        for (char c: String.valueOf(second).toCharArray()) {
+            secondList.add(c - '0');
+        }
+
+        secondList.sort(Collections.reverseOrder());
+
+        int indx = 0;
+
+        for (int i = 0; i < firstChar.length; i++) {
+            if (indx < secondList.size() && secondList.get(indx) > (firstChar[i] - '0')) {
+                firstChar[i] = (char) (secondList.get(indx) + '0');
+                indx++;
+            }
+        }
+
+        return Integer.parseInt(new String(firstChar));
+
+    }
+
+    public static String timeDifference(String cityA, String timestamp, String cityB) {
+        HashMap<String, Integer> cityOffsets = new HashMap<>();
+        cityOffsets.put("Los Angeles", -8 * 60);
+        cityOffsets.put("New York", -5 * 60);
+        cityOffsets.put("Caracas", -4 * 60 - 30);
+        cityOffsets.put("Buenos Aires", -3 * 60);
+        cityOffsets.put("London", 0);
+        cityOffsets.put("Rome", 1 * 60);
+        cityOffsets.put("Moscow", 3 * 60);
+        cityOffsets.put("Tehran", 3 * 60 + 30);
+        cityOffsets.put("New Delhi", 5 * 60 + 30);
+        cityOffsets.put("Beijing", 8 * 60);
+        cityOffsets.put("Canberra", 10 * 60);
+
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy HH:mm", Locale.US);
+        LocalDateTime timeInCityA = LocalDateTime.parse(timestamp, inputFormatter);
+
+        int offsetA = cityOffsets.get(cityA);
+        int offsetB = cityOffsets.get(cityB);
+
+        int offsetDifference = offsetB - offsetA;
+
+        LocalDateTime timeInCityB = timeInCityA.plusMinutes(offsetDifference);
+
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-M-d HH:mm");
+        return timeInCityB.format(outputFormatter);
+    }
+
+    public static boolean isNew(int num) {
+        String numStr = String.valueOf(num);
+        Set<Integer> permutations = new HashSet<>();
+
+        generatePermutations("", numStr, permutations);
+
+        for (int perm : permutations) {
+            if (perm < num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void generatePermutations(String prefix, String remaining, Set<Integer> result) {
+        if (remaining.isEmpty()) {
+            if (!prefix.startsWith("0")) {
+                result.add(Integer.parseInt(prefix));
+            }
+        } else {
+            for (int i = 0; i < remaining.length(); i++) {
+                generatePermutations(
+                        prefix + remaining.charAt(i),
+                        remaining.substring(0, i) + remaining.substring(i + 1),
+                        result
+                );
+            }
+        }
+    }
+
+    public static int totalPoints(String[] guessedWords, String unscrambledWord) {
+        int totalPoints = 0;
+
+        for (String word : guessedWords) {
+            if (isValid(word, unscrambledWord)) {
+                int wordPoints = calculatePoints(word);
+                totalPoints += wordPoints;
+            }
+        }
+
+        return totalPoints;
+    }
+
+    private static boolean isValid(String word, String unscrambledWord) {
+        Map<Character, Integer> letterCount = getLetterCount(unscrambledWord);
+
+        for (char c : word.toCharArray()) {
+            letterCount.put(c, letterCount.getOrDefault(c, 0) - 1);
+            if (letterCount.get(c) < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Map<Character, Integer> getLetterCount(String word) {
+        Map<Character, Integer> countMap = new HashMap<>();
+
+        for (char c : word.toCharArray()) {
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        }
+
+        return countMap;
+    }
+
+    private static int calculatePoints(String word) {
+        int wordLength = word.length();
+
+        if (wordLength == 3) return 1;
+        if (wordLength == 4) return 2;
+        if (wordLength == 5) return 3;
+        if (wordLength == 6) return 4 + 50;
+
+        return 0;
+    }
 }
